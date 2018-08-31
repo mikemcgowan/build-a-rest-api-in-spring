0a78ed7446c288ff32f993aa336a38de60c15edd
Mike McGowan <mike.mcgowan@leolearning.com>
2018-08-31 16:51:46 +0100

    End of https://teamtreehouse.com/library/entities-and-repositories-review

---
+++ src/main/java/com/teamtreehouse/review/Review.java
@@ -0,0 +1,45 @@
+package com.teamtreehouse.review;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Review {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private final Long id;
+
+  private int rating;
+
+  private String description;
+
+  protected Review() {
+    id = null;
+  }
+
+  public Review(int rating, String description) {
+    this();
+    this.rating = rating;
+    this.description = description;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public void setRating(int rating) {
+    this.rating = rating;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+}
