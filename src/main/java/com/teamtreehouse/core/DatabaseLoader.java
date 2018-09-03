482c747c49b05c4ab2ae82b9ada9dd56f2e6908b
Mike McGowan <mike.mcgowan@leolearning.com>
2018-09-03 14:53:16 +0100

    End of https://teamtreehouse.com/library/adding-users

--- src/main/java/com/teamtreehouse/core/DatabaseLoader.java
+++ src/main/java/com/teamtreehouse/core/DatabaseLoader.java
@@ -3,12 +3,15 @@ package com.teamtreehouse.core;
 import com.teamtreehouse.course.Course;
 import com.teamtreehouse.course.CourseRepository;
 import com.teamtreehouse.review.Review;
+import com.teamtreehouse.user.User;
+import com.teamtreehouse.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -16,10 +19,12 @@ import java.util.stream.IntStream;
 public class DatabaseLoader implements ApplicationRunner {
 
   private final CourseRepository courses;
+  private final UserRepository users;
 
   @Autowired
-  public DatabaseLoader(CourseRepository courses) {
+  public DatabaseLoader(CourseRepository courses, UserRepository users) {
     this.courses = courses;
+    this.users = users;
   }
 
   @Override
@@ -44,6 +49,30 @@ public class DatabaseLoader implements ApplicationRunner {
       "Spring REST Data",
       "Spring HATEOAS",
     };
+
+    List<User> students = Arrays.asList(
+      new User("jacobproffer", "Jacob",  "Proffer", "password", new String[] {"ROLE_USER"}),
+      new User("mlnorman", "Mike",  "Norman", "password", new String[] {"ROLE_USER"}),
+      new User("k_freemansmith", "Karen",  "Freeman-Smith", "password", new String[] {"ROLE_USER"}),
+      new User("seth_lk", "Seth",  "Kroger", "password", new String[] {"ROLE_USER"}),
+      new User("mrstreetgrid", "Java",  "Vince", "password", new String[] {"ROLE_USER"}),
+      new User("anthonymikhail", "Tony",  "Mikhail", "password", new String[] {"ROLE_USER"}),
+      new User("boog690", "AJ",  "Teacher", "password", new String[] {"ROLE_USER"}),
+      new User("faelor", "Erik",  "Faelor Shafer", "password", new String[] {"ROLE_USER"}),
+      new User("christophernowack", "Christopher",  "Nowack", "password", new String[] {"ROLE_USER"}),
+      new User("calebkleveter", "Caleb",  "Kleveter", "password", new String[] {"ROLE_USER"}),
+      new User("richdonellan", "Rich",  "Donnellan", "password", new String[] {"ROLE_USER"}),
+      new User("albertqerimi", "Albert",  "Qerimi", "password", new String[] {"ROLE_USER"})
+    );
+    users.saveAll(students);
+    users.save(new User(
+      "Mike",
+      "McGowan",
+      "mike.mcgowan",
+      "Wibble123!",
+      new String[] {"ROLE_USER", "ROLE_ADMIN"}
+    ));
+
     List<Course> cs = new ArrayList<>();
     IntStream.range(0, 100)
       .forEach(i -> {
@@ -51,7 +80,9 @@ public class DatabaseLoader implements ApplicationRunner {
         String buzzword = buzzwords[i % buzzwords.length];
         String title = String.format(template, buzzword);
         Course c = new Course(title, "https://teamtreehouse.com/library/java-basics");
-        c.addReview(new Review(i % 5, String.format("Moar %s please!!!", buzzword)));
+        Review review1 = new Review(1 + (i % 5), String.format("Moar %s please!!!", buzzword));
+        review1.setReviewer(students.get(i % students.size()));
+        c.addReview(review1);
         cs.add(c);
       });
     courses.saveAll(cs);
--- src/main/java/com/teamtreehouse/review/Review.java
+++ src/main/java/com/teamtreehouse/review/Review.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.review;
 
 import com.teamtreehouse.core.BaseEntity;
 import com.teamtreehouse.course.Course;
+import com.teamtreehouse.user.User;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -14,6 +15,9 @@ public class Review extends BaseEntity {
 
   @ManyToOne
   private Course course;
+
+  @ManyToOne
+  private User reviewer;
 
   protected Review() {
     super();
@@ -47,6 +51,14 @@ public class Review extends BaseEntity {
 
   public void setCourse(Course course) {
     this.course = course;
+  }
+
+  public User getReviewer() {
+    return reviewer;
+  }
+
+  public void setReviewer(User reviewer) {
+    this.reviewer = reviewer;
   }
 
 }
