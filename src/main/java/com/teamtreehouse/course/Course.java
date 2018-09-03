541c3ef9c138e87be7430a0c4d94730f8852b112
Mike McGowan <mike.mcgowan@leolearning.com>
2018-09-03 12:53:24 +0100

    End of https://teamtreehouse.com/library/validation-2

--- src/main/java/com/teamtreehouse/course/Course.java
+++ src/main/java/com/teamtreehouse/course/Course.java
@@ -6,13 +6,18 @@ import com.teamtreehouse.review.Review;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Course extends BaseEntity {
 
+  @NotNull
+  @Size(min = 2, max = 140)
   private String title;
+
   private String url;
 
   @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
